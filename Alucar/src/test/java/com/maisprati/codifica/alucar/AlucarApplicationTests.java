5f88558f985d1a42ddf626aebe215be313bf23cc
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-12-19 07:56:17 -0300

    Structure(Dependencies/Structural): :sparkles: Creating Initializer

---
+++ Alucar/src/test/java/com/maisprati/codifica/alucar/AlucarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maisprati.codifica.alucar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlucarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
